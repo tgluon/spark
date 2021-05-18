208b38353980f2fef05ba7b3174a113aa2b1c032
tangxiuhong <tangxiuhong@cangshu.vip>
2021-05-18 11:52:41 +0800

    给ParserUtils添加注释

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/parser/ParserUtils.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/parser/ParserUtils.scala
@@ -29,6 +29,7 @@ import org.apache.spark.sql.catalyst.trees.{CurrentOrigin, Origin}
 
 /**
  * A collection of utility methods for use during the parsing process.
+ * 创建获取token的命令
  */
 object ParserUtils {
   /** Get the command which created the token. */
@@ -37,25 +38,54 @@ object ParserUtils {
     stream.getText(Interval.of(0, stream.size() - 1))
   }
 
+  /**
+   * 非法操作异常处理类
+   *
+   * @param message 传入一段信息和解析规则
+   * @param ctx     解析规则上下文
+   * @return 抛出解析非法操作异常 Opration not allowed + 具体信息
+   */
   def operationNotAllowed(message: String, ctx: ParserRuleContext): Nothing = {
     throw new ParseException(s"Operation not allowed: $message", ctx)
   }
 
+  /**
+   * 校验多个表示异常处理类
+   *
+   * @param nodes      节点集合，表达式，解析规则
+   * @param clauseName 子句名称
+   * @param ctx        解析规则上下文
+   * @tparam T 抛出解析多表达式异常“Found duplicate clauses:”+具体表达式
+   */
   def checkDuplicateClauses[T](
-      nodes: util.List[T], clauseName: String, ctx: ParserRuleContext): Unit = {
+                                nodes: util.List[T], clauseName: String, ctx: ParserRuleContext): Unit = {
     if (nodes.size() > 1) {
       throw new ParseException(s"Found duplicate clauses: $clauseName", ctx)
     }
   }
 
-  /** Check if duplicate keys exist in a set of key-value pairs. */
+  /**
+   * Check if duplicate keys exist in a set of key-value pairs.
+   * 校验是否包含多个重复的key  比如AstBuilder中withCTE中在生成LogicalPlan前做了校验
+   *
+   * @param keyPairs 组KEY-VALUE集合，解析规则
+   * @param ctx      解析规则上下文
+   * @tparam T 抛出解析重复key异常“Found duplicate keys”+key
+   */
   def checkDuplicateKeys[T](keyPairs: Seq[(String, T)], ctx: ParserRuleContext): Unit = {
     keyPairs.groupBy(_._1).filter(_._2.size > 1).foreach { case (key, _) =>
       throw new ParseException(s"Found duplicate keys '$key'.", ctx)
     }
   }
 
-  /** Get the code that creates the given node. */
+
+  /**
+   * Get the code that creates the given node.
+   * 根据已有的节点，创建指定代码
+   *
+   * @param ctx 创建接续规则上下文
+   * @return 根据解析规则上下文开始index和结束index，创建stream代码
+   */
   def source(ctx: ParserRuleContext): String = {
     val stream = ctx.getStart.getInputStream
     stream.getText(Interval.of(ctx.getStart.getStartIndex, ctx.getStop.getStopIndex))
@@ -64,20 +94,47 @@ object ParserUtils {
   /** Get all the text which comes after the given rule. */
   def remainder(ctx: ParserRuleContext): String = remainder(ctx.getStop)
 
-  /** Get all the text which comes after the given token. */
+  /**
+   * Get all the text which comes after the given token.
+   * 根据已有的节点，创建所有文本
+   *
+   * @param token 解析规则上下文
+   * @return 上下文，具体逻辑直接调用remainder方法，详细看remainder方式
+   */
   def remainder(token: Token): String = {
     val stream = token.getInputStream
     val interval = Interval.of(token.getStopIndex + 1, stream.size() - 1)
     stream.getText(interval)
   }
 
-  /** Convert a string token into a string. */
+
+  /**
+   * Convert a string token into a string.
+   * 字符串类型的token转换为scala字符串
+   *
+   * @param token
+   * @return 对应字符串  核心看unescapeSQLString方法
+   */
   def string(token: Token): String = unescapeSQLString(token.getText)
 
-  /** Convert a string node into a string. */
+
+  /**
+   * Convert a string node into a string.
+   * 字符串类型node转为scala字符串
+   *
+   * @param node 字符串类型TerminalNode
+   * @return 返回对应字符串  核心看unescapeSQLString方法
+   */
   def string(node: TerminalNode): String = unescapeSQLString(node.getText)
 
-  /** Convert a string node into a string without unescaping. */
+
+  /**
+   * Convert a string node into a string without unescaping.
+   * 把字符串类型node转为非修饰的字符创,方法描述也很详细，就是解析规则强制要求输入一定要在开始结束之间要有引用标识
+   *
+   * @param node
+   * @return
+   */
   def stringWithoutUnescape(node: TerminalNode): String = {
     // STRING parser rule forces that the input always has quotes at the starting and ending.
     node.getText.slice(1, node.getText.size - 1)
@@ -88,13 +145,27 @@ object ParserUtils {
     Option(value).toSeq.map(x => key -> string(x))
   }
 
-  /** Get the origin (line and position) of the token. */
+
+  /**
+   * Get the origin (line and position) of the token.
+   * 获取一个token的位置信息
+   *
+   * @param token
+   * @return
+   */
   def position(token: Token): Origin = {
     val opt = Option(token)
     Origin(opt.map(_.getLine), opt.map(_.getCharPositionInLine))
   }
 
-  /** Validate the condition. If it doesn't throw a parse exception. */
+  /**
+   * Validate the condition. If it doesn't throw a parse exception.
+   * 这个就比较简单的校验条件是否合法，如果不合法就抛出解析异常
+   *
+   * @param f
+   * @param message
+   * @param ctx
+   */
   def validate(f: => Boolean, message: String, ctx: ParserRuleContext): Unit = {
     if (!f) {
       throw new ParseException(message, ctx)
@@ -105,6 +176,9 @@ object ParserUtils {
    * Register the origin of the context. Any TreeNode created in the closure will be assigned the
    * registered origin. This method restores the previously set origin after completion of the
    * closure.
+   * 注册解析规则上下文的来源。
+   * 在闭环中创建的任何树节点都将被指定为已注册的源节点。
+   * 此方法在关闭完成后恢复先前设置的原点。
    */
   def withOrigin[T](ctx: ParserRuleContext)(f: => T): T = {
     val current = CurrentOrigin.get
@@ -120,7 +194,7 @@ object ParserUtils {
   def unescapeSQLString(b: String): String = {
     var enclosure: Character = null
     val sb = new StringBuilder(b.length())
-
+  // 字符转换并赋值到sb数组中
     def appendEscapedChar(n: Char): Unit = {
       n match {
         case '0' => sb.append('\u0000')
@@ -197,11 +271,15 @@ object ParserUtils {
   /** the column name pattern in quoted regex with qualifier */
   val qualifiedEscapedIdentifier = ("(.+)" + """.""" + "`(.+)`").r
 
-  /** Some syntactic sugar which makes it easier to work with optional clauses for LogicalPlans. */
+  /**
+   * Some syntactic sugar which makes it easier to work with optional clauses for LogicalPlans.
+   *  一些语法糖，使逻辑计划的可选从句更容易使用
+   */
   implicit class EnhancedLogicalPlan(val plan: LogicalPlan) extends AnyVal {
     /**
      * Create a plan using the block of code when the given context exists. Otherwise return the
      * original plan.
+     * 当给定的上下文存在时，使用代码块创建计划。否则返回原始计划
      */
     def optional(ctx: AnyRef)(f: => LogicalPlan): LogicalPlan = {
       if (ctx != null) {
@@ -214,6 +292,7 @@ object ParserUtils {
     /**
      * Map a [[LogicalPlan]] to another [[LogicalPlan]] if the passed context exists using the
      * passed function. The original plan is returned when the context does not exist.
+     * 逻辑计划转换，若旧的上下文解析规则存在就使用旧的方法，否则就返回原始计划
      */
     def optionalMap[C](ctx: C)(f: (C, LogicalPlan) => LogicalPlan): LogicalPlan = {
       if (ctx != null) {
